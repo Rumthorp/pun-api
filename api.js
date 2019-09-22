cf35cdead3a087e6bcc877e7c2c469eb58d748be
Rumthorp <mattmerwinjennings@gmail.com>
2019-09-22 04:06:12 -0400

    enable cors

--- api.js
+++ api.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 const router = require('./routes/router');
 
+app.use(cors());
 app.use(router);
 app.listen(port);
